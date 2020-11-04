d557a26dbdabcf1330d1a50fec3111792f0664d1
Anne <anne.k.plummer@gmail.com>
2020-11-03 21:22:17 -0600

    password option prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,77 @@
 // Assignment code here
+var lowercaseCharacters = ['a','b', 'c', 'd', 'e','f','g','h', 'i', 'j','k','l', 'm', 'n', 'o', 'p', 'q', 'r','s','t','u','v','w','x','y','z'];
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters =['!', '@', '#', '$', '%', '^', '&', '*', '(', ')','-', '_', '+', '=', '~', "'",':', ';', '.', '{', '}', '[', ']', '?', '/', '\\', '|']
+
+// Function for password options prompt
+function passwordOptions () {
+  // Password length
+  var length = parseInt (
+    prompt ("How many characters long would you like your password to be? 8 to 128 characters")
+  );
+
+  if (length >= 8 && length <= 128){
+    window.alert("Character length is" + length);
+  }
+  else {
+    window.alert("Please enter a number between 8 and 128.");
+    return;
+  }
+
+  var specialChar = confirm(
+    "Would you like to have special characters in your password?"
+  );
+
+  var numericChar = confirm(
+    "Would you like numbers in your password?"
+  );
+
+  var lowerChar = confirm(
+    "Would you like lowercase letters in your password?"
+  );
+
+  var upperChar = confirm(
+    "Would you like uppercase letters in your password?"
+  );
+
+  if (
+    specialChar === false &&
+    numericChar === false &&
+    lowerChar === false &&
+    upperChar === false
+  ) {
+    alert("You must select at least one character type.");
+    return;
+  }
+
+  var passwordOptions = {
+    length: length,
+    specialChar: specialChar,
+    numericChar: numericChar,
+    lowerChar: lowerChar,
+    upperChar: upperChar
+  };
+
+  return passwordOptions;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
